3c07911adb3e6365190c2fa50d6bf3d0093a0716
Emanuell Santos <dfmanu06@gmail.com>
2020-03-17 17:23:10 -0300

    Criando método testFindByEmail e alguns ajustes

--- src/main/java/br/com/manell/wallet/domains/User.java
+++ src/main/java/br/com/manell/wallet/domains/User.java
@@ -1,12 +1,16 @@
 package br.com.manell.wallet.domains;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
--- src/test/java/br/com/manell/wallet/repository/UserRepositoryTest.java
+++ src/test/java/br/com/manell/wallet/repository/UserRepositoryTest.java
@@ -2,6 +2,8 @@ package br.com.manell.wallet.repository;
 
 import br.com.manell.wallet.domains.User;
 import br.com.manell.wallet.repositories.UserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,26 +11,47 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @ActiveProfiles("test")
 public class UserRepositoryTest {
 
+    private static final String EMAIL = "admin@admin.com";
+
     @Autowired
     private UserRepository userRepository;
+
+    @BeforeEach
+    public void setUp() {
+
+        this.testSave();
+    }
+
+    @AfterEach
+    public void tearDown() {
+
+        userRepository.deleteAll();
+    }
 
     @Test
     public void testSave() {
 
-        User user = new User();
-        user.setName("Teste");
-        user.setPassword("admin");
-        user.setEmail("teste@email.com");
-
+        User user = new User(null, "Teste", EMAIL, "admin");
         User response = userRepository.save(user);
 
         assertNotNull(response);
+    }
+
+    @Test
+    public void testFindByEmail() {
+
+        User response = userRepository.findByEmail(EMAIL);
+
+        assertTrue(Objects.nonNull(response));
+        assertEquals(response.getEmail(), EMAIL);
     }
 }
