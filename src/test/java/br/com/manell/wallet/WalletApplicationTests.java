9d3ae61aa939ee85f8860ed120cd1a5ab65e6bf1
Emanuell Santos <dfmanu06@gmail.com>
2020-03-17 13:55:31 -0300

    Commit Inicial

---
+++ src/test/java/br/com/manell/wallet/WalletApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.manell.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
